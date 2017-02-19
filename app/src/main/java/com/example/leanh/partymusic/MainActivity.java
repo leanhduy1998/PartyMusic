16029acee9e59251b5779fc75e10630741384af9
Duy Le <Duy Le>
2017-02-19 01:07:43 -0600

    update stuffs

--- app/src/main/java/com/example/leanh/partymusic/MainActivity.java
+++ app/src/main/java/com/example/leanh/partymusic/MainActivity.java
@@ -4,27 +4,36 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView mainPic;
+    private Button joinBtn;
+    private Button hostBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mainPic = (ImageView)findViewById(R.id.mainPic);
-        mainPic.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
-            public void onSwipeRight() {
+        joinBtn= (Button) findViewById(R.id.joinButton);
+        hostBtn= (Button) findViewById(R.id.hostButton);
+
+        joinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent hostSessionIntent = new Intent(MainActivity.this, HostSession.class);
                 startActivity(hostSessionIntent);
             }
-            public void onSwipeLeft() {
+        });
+
+        hostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent joinSessionIntent = new Intent(MainActivity.this, JoinSession.class);
                 startActivity(joinSessionIntent);
             }
-
         });
 
     }
